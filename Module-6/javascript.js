66585cc02860d727e49e0b500455942c42bc635f
ggeorge <gmgeorge1993@gmail.com>
2019-08-18 10:44:35 -0500

    added function makeBoxFade

--- Module-6/javascript.js
+++ Module-6/javascript.js
@@ -12,14 +12,16 @@
             document.getElementById("box").style.backgroundColor = "blue";
         }
 
+        function makeBoxFade(){
+            document.getElementById("box").style.transition ="opacity 2s";
+            document.getElementById("box").style.opacity = 0;
+        }
         document.getElementById("button1").addEventListener("click", makeBoxBig);
 
         document.getElementById("button2").addEventListener("click", makeBoxBlue);
         
-        document.getElementById("button3").addEventListener("click", function(){
-            document.getElementById("box").style.transition ="opacity 2s";
-            document.getElementById("box").style.opacity = 0;
-        });
+        document.getElementById("button3").addEventListener("click", makeBoxFade);
+            
         
         document.getElementById("button4").addEventListener("click", function(){
             document.getElementById("box").style.height = "150px";

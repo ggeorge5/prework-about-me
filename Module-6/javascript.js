414931f2c97c949493eb5e03807f382d675c4d62
ggeorge <gmgeorge1993@gmail.com>
2019-08-18 10:41:26 -0500

    Created function makeBoxBig

--- Module-6/javascript.js
+++ Module-6/javascript.js
@@ -1,19 +1,27 @@
 
-        function myFunction() {
+        function adjustFontSizeAndColor() {
             document.getElementById("demo").style.color = "red";
             document.getElementById("demo").style.fontSize = "25px";
         }
-            document.getElementById("button1").addEventListener("click", function(){
+
+        function makeBoxBig(){
             document.getElementById("box").style.height = "250px";
-        });
-            document.getElementById("button2").addEventListener("click", function(){
+        }
+
+        function makeBoxBlue(){
             document.getElementById("box").style.backgroundColor = "blue";
-        });
-            document.getElementById("button3").addEventListener("click", function(){
+        }
+
+        document.getElementById("button1").addEventListener("click", makeBoxBig);
+
+        document.getElementById("button2").addEventListener("click", makeBoxBlue);
+        
+        document.getElementById("button3").addEventListener("click", function(){
             document.getElementById("box").style.transition ="opacity 2s";
             document.getElementById("box").style.opacity = 0;
         });
-            document.getElementById("button4").addEventListener("click", function(){
+        
+        document.getElementById("button4").addEventListener("click", function(){
             document.getElementById("box").style.height = "150px";
             document.getElementById("box").style.width = "150px";
             document.getElementById("box").style.backgroundColor = "orange";
